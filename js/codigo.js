cc6b598a184617a9f1d8e61a80e572a845b62ab4
Andruzz <avallelisboa@gmail.com>
2020-02-24 11:39:28 -0300

    drawbranchesintomap

--- js/codigo.js
+++ js/codigo.js
@@ -1,26 +1,26 @@
 ons.ready(()=>{
-  const navigator = document.querySelector("#navigator");
-  if(!isLogged()) navigator.resetToPage('login.html');
+  const navigatorComponent = document.querySelector("#navigator");
+  if(!isLogged()) navigatorComponent.resetToPage('login.html');
   else {
-    navigator.resetToPage('home.html');
-    
+    navigatorComponent.resetToPage('home.html');
+    getProducts();
   }
 });
 
-document.addEventListener('init',(event)=>{
+document.addEventListener('init', (event)=>{
   var id = event.target.id;
-  if(id =="home" || id == "map")
+  if(id =="home" || id == "map"){
     document.getElementById("userEmail").innerText = "Bienvenido " + localStorage.getItem("email");
+  }
 });
-
 function index(){
-  const navigator = document.querySelector("#navigator");
-  navigator.resetToPage('login.html');
+  const navigatorComponent = document.querySelector("#navigator");
+  navigatorComponent.resetToPage('login.html');
 }
 
 function gotoRegister(){
-  const navigator = document.querySelector("#navigator");
-  navigator.resetToPage('register.html');
+  const navigatorComponent = document.querySelector("#navigator");
+  navigatorComponent.resetToPage('register.html');
 }
 
 function register(){
@@ -51,8 +51,8 @@ function register(){
 
 function continueRegisteredPopOver(){
   $("#userRegisteredPopOver").hide();
-  const navigator = document.querySelector("#navigator");
-  navigator.resetToPage('login.html');
+  const navigatorComponent = document.querySelector("#navigator");
+  navigatorComponent.resetToPage('login.html');
 }
 
 function login(){
@@ -73,13 +73,7 @@ function login(){
 
   else{
     loginService(email, password, ()=>{
-      const navigator = document.querySelector("#navigator");
-      getProductsService((products)=>{
-          products.forEach((element)=>{
-            console.log(element);
-          });
-      });
-      navigator.resetToPage('home.html');
+      goToProducts();
       document.addEventListener('init',(event)=>{
         if(event.target.id =="home")
           document.getElementById("userEmail").innerText = "Bienvenido " + localStorage.getItem("email");
@@ -88,31 +82,125 @@ function login(){
   }
 }
 
-function goToMap(){
-  const navigator = document.querySelector("#navigator");
-  navigator.resetToPage('map.html');
+function getProducts(search){
+  getProductsService((products)=>{
+    $("#productsList").html('');
+    $("#productsList").append(`                                    
+                              <tr>
+                                <th></th>
+                                <th>Nombre</th>
+                                <th>Precio</th>
+                                <th>Número de sucursales</th>
+                                <th>Descripción</th>                                
+                              </tr>`
+                            );
+      products.forEach((element)=>{
+        console.log(element);
+        if(search == null){
+          $("#productsList").append(`
+          <tr>
+            <td><img src="${element.photo}" width="100" height="100"></td>
+            <td>${element.name}</td>
+            <td>${element.price}</td>
+            <td>${element.branches.length}</td>
+            <td>${element.description}</td>            
+            <td><ons-button onClick='goToMap(${JSON.stringify(element.branches)})'>Ver sucursales</ons-button></td>
+          </tr>
+        `);
+        }
+        else{
+          if(element.name.toLowerCase().includes(search.selector.toLowerCase()))
+              $("#productsList").append(`
+                                    <tr>
+                                      <td><img src="${element.photo}" width="100" height="100"></td>
+                                      <td>${element.name}</td>
+                                      <td>${element.price}</td>
+                                      <td>${element.branches.length}</td>
+                                      <td>${element.description}</td>                                      
+                                      <td><ons-button onClick='goToMap(${JSON.stringify(element.branches)})'>Ver sucursales</ons-button></td>
+                                    </tr>
+                                  `);
+        }      
+      });
+  });
 }
 
 function goToProducts(){
-  const navigator = document.querySelector("#navigator");
-  navigator.resetToPage('home.html');
+  const navigatorComponent = document.querySelector("#navigator");
+  navigatorComponent.resetToPage('home.html');
+  getProducts();
 }
 
 
 function logout(){
   localStorage.clear();
-  const navigator = document.querySelector("#navigator");
-  navigator.resetToPage('login.html');
+  const navigatorComponent = document.querySelector("#navigator");
+  navigatorComponent.resetToPage('login.html');
+}
+
+function drawMap(branches){
+    window.navigator.geolocation.getCurrentPosition(function(pos){
+    var crd = pos.coords;
+    var map = L.map("mapid").setView([crd.latitude, crd.longitude], 15);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    if(branches != null){
+        branches.forEach(function(suc){
+          if(distance(crd.latitude, crd.longitude, suc.latitude, suc.longitude,"K") < 10){
+            L.marker([suc.latitude, suc.longitude]).addTo(map)
+            .bindPopup(`${suc.name}`);
+          }
+        });
+    }
+  }, function(err){
+      ons.navigator.toast("No se pudo obtener su ubicación. Se mostrarán todas las sucursales.",{"timeout":3000});
+      var map = L.map("mapid").setView([-34.8511158, -56.1232877], 12);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+      products.forEach(function(prod){
+        prod.branches.forEach(function(suc){
+          L.marker([suc.latitude, suc.longitude]).addTo(map)
+          .bindPopup(`${suc.name}`);
+        })
+      });
+  }, {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  });
+}
+
+function goToMap(_branches){
+  var branches = _branches;
+  const navigatorComponent = document.querySelector("#navigator");
+  navigatorComponent.resetToPage('map.html');
+  drawMap(branches);
 }
 
 
-document.addEventListener('init', function(event) {
-  var page = event.target;
+function distance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
+}
 
-  if (page.id === 'map') {
-    var map = L.map('mapid').setView([-34.7968596, -56.0694617], 15);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  } 
-});
